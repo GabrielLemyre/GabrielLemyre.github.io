aecc16da46779ace48936df52b2da02f52e136e8
GabrielLemyre <gabriel@lemyre.org>
2022-02-05 23:44:16 -0500

    Updated image size

--- VinyleTable.R
+++ VinyleTable.R
@@ -34,7 +34,7 @@ Albums.info.full.Ordered <- Albums.info.full[order(Albums.info.full[["Artist"]],
 
 tableWeb <- paste("<tr>\n",
             "\t<!-- Path to the image -->\n",
-            "\t<td><img src=\"",Albums.info.full.Ordered[["Path"]],"\" alt=\"",Albums.info.full.Ordered[["Path"]],"\" style='border:2px solid #FFFFFF' height=200 width=200></img></th>\n",
+            "\t<td><img src=\"",Albums.info.full.Ordered[["Path"]],"\" alt=\"",Albums.info.full.Ordered[["Path"]],"\" style='border:2px solid #FFFFFF' height=200></img></th>\n",
             "\t<td>",Albums.info.full.Ordered[["Album"]],"</th>\n",
             "\t<td>",Albums.info.full.Ordered[["Artist"]],"</th>\n",
             "\t<td>","","</th>\n",
