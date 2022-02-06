4976dcfbd8834375f5517834fb2d6890a8a1e9a4
GabrielLemyre <gabriel@lemyre.org>
2022-02-06 00:28:30 -0500

    REsolved issue with special characters in image paths

--- VinyleTable.R
+++ VinyleTable.R
@@ -2,6 +2,9 @@
 # 
 # install.packages("htmltools")
 library(htmltools)
+library(writexl)
+library(stringr)
+library(stringi)
 
 chr <- function(n) { rawToChar(as.raw(n)) }
 
@@ -19,8 +22,9 @@ Copie.Presse.Papier <- function(string) {
       }
 }
 
-path.to.images.folder <- "/Users/gabriellemyre/Documents/GitHub/GabrielLemyre.github.io/images"
-albums <- list.files(path.to.images.folder)
+path.to.images.folder.Old <- "/Users/gabriellemyre/Documents/GitHub/GabrielLemyre.github.io/images INPUT"
+path.to.images.folder <- "/Users/gabriellemyre/Documents/GitHub/GabrielLemyre.github.io/images OUTPUT"
+albums <- list.files(path.to.images.folder.Old)
 
 albums <- matrix(albums,length(albums),1)
 
@@ -28,13 +32,25 @@ Albums.noExt <- unlist(strsplit(albums,".jpg"))
 
 Albums.info <- do.call(rbind, strsplit(Albums.noExt," - "))
 
-Albums.info.full <- data.table(cbind(Albums.info,paste("images\\",albums,sep="")))
+Albums.info.full <- data.table(cbind(Albums.info,albums))
 names(Albums.info.full) <- c("Artist","Album","Path")
+
+Albums.info.full[, Path := stri_trans_general(str = Path, 
+                                   id = "Latin-ASCII")]
+Albums.info.full$Path <- gsub("\\?", "", Albums.info.full$Path)
+Albums.info.full$Path <- gsub("\\'", "", Albums.info.full$Path)
+
+file.rename(paste(path.to.images.folder,list.files(path.to.images.folder),sep="/"), paste(path.to.images.folder,Albums.info.full[["Path"]],sep="/"))
+
+# Order
 Albums.info.full.Ordered <- Albums.info.full[order(Albums.info.full[["Artist"]],Albums.info.full[["Album"]]),]
+
+write_xlsx(Albums.info.full.Ordered,paste("/Users/gabriellemyre/Documents/GitHub/GabrielLemyre.github.io/VinyleTable.xlsx"))
+file.rename(list.files(path.to.images.folder), str_replace(list.files(path.to.images.folder),pattern = "?", ""))
 
 tableWeb <- paste("<tr>\n",
             "\t<!-- Path to the image -->\n",
-            "\t<td><img src=\"",Albums.info.full.Ordered[["Path"]],"\" alt=\"",Albums.info.full.Ordered[["Album"]],"\" style='border:2px solid #FFFFFF' height=200></img></th>\n",
+            "\t<td><img src=\"images OUTPUT\\",Albums.info.full.Ordered[["Path"]],"\" alt=\"",Albums.info.full.Ordered[["Album"]],"\" style='border:2px solid #FFFFFF' height=200></img></th>\n",
             "\t<td>",Albums.info.full.Ordered[["Album"]],"</th>\n",
             "\t<td>",Albums.info.full.Ordered[["Artist"]],"</th>\n",
             "\t<td>","","</th>\n",
